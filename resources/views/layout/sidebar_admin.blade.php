97a0ae921a1dfcbb07b156343fb335e7fddf7227
magang 5 UDI <fauziardiansyah302@gmail.com>
2024-10-15 15:54:06 +0700

    fix sidebar

--- resources/views/layout/sidebar_admin.blade.php
+++ resources/views/layout/sidebar_admin.blade.php
@@ -30,25 +30,7 @@
 </aside>
 
 <div class="relative w-full flex flex-col h-screen overflow-y-hidden">
-    <!-- Desktop Header -->
-    <header class="w-full justify-between items-center bg-white py-2 px-2 md:px-10 flex">
-        <h1 class="text-3xl font-semibold">{{$pageTitle}}</h1>
-        <div x-data="{ isOpen: false }" class="relative w-1/2 flex justify-end">
-            <button @click="isOpen = !isOpen"
-                class="relative z-10 rounded-full overflow-hidden hover:cursor-default  focus:border-gray-300 focus:outline-none">
-                <div class="flex justify-center py-1 px-3 items-center bg-[#f2f2f3] rounded-md text-white">
-                    <i
-                        class="fa-regular fa-circle-user  text-4xl text-[#718096] hover:text-blue-700 transition font-medium duration-500 "></i>
-                    <div class="flex items-start flex-col text-black md:text-md py-1 px-2">
-                        <small class="font-bold">Ardiansyah</small>
-                        <small class="text-[#718096]">Admin</small>
-                    </div>
-                </div>
-            </button>
-        
-        </div>
-    </header>
-
+    
     <!-- Mobile Header & Nav -->
     <header x-data="{ isOpen: false }" class="w-full bg-sidebar py-5 px-6 sm:hidden">
         <div class="flex items-center justify-between">
@@ -61,6 +43,18 @@
 
         <!-- Dropdown Nav -->
         <nav :class="isOpen ? 'flex' : 'hidden'" class="flex flex-col pt-4">
+            <button @click="isOpen = !isOpen"
+            class="relative z-10 rounded-full overflow-hidden hover:cursor-default  focus:border-gray-300 focus:outline-none">
+            <div class="flex justify-start py-1 px-3 items-center bg-[#f2f2f3] rounded-md text-white">
+                <i
+                    class="fa-regular fa-circle-user  text-4xl text-[#718096] hover:text-blue-700 transition font-medium duration-500 "></i>
+                <div class="flex items-start flex-col text-black md:text-md py-1 px-2">
+                    <small class="font-bold">Ardiansyah</small>
+                    <small class="text-[#718096]">Admin</small>
+                </div>
+            </div>
+        </button>
+    
             <a href="{{route('admin.user.index')}}" class="flex items-center  opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
     <i class='mr-3'><img src="{{asset('user-icon.png')}}" alt=""></i>
                     Manajemen User
@@ -78,11 +72,24 @@
 
                 Forum Konsultasi
             </a>
-          
-            <button id="logoutButton"
-                class="w-full bg-white font-semibold py-2 mt-3 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-300 flex items-center justify-center">
-                <i class="mr-3"><img src="{{asset("logout-icon.png")}}" alt=""></i> 
+            <button id="logoutButton" class="flex items-center  opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
+                <i class="mr-3"><img src="{{asset("logout-icon.png")}}" alt=""></i> Logout
+
             </button>
+          
         </nav>
 
     </header>
+<!-- Desktop Header -->
+<header class="w-full hidden md:flex md:justify-between justify-center items-center md:bg-white py-2 px-2 md:px-10 flex">
+    <h1 class="text-xl md:text-3xl font-semibold ">{{$pageTitle}}</h1>
+            <div class="flex justify-center py-1 px-3 items-center bg-[#f2f2f3] rounded-md text-white">
+                <i
+                    class="fa-regular fa-circle-user  text-xl md:text-4xl text-[#718096] hover:text-blue-700 transition font-medium duration-500 "></i>
+                <div class="flex items-start flex-col text-black text-sm md:text-md md:py-1 px-1 md:px-2">
+                    <small class="font-bold">Ardiansyah</small>
+                    <small class="text-[#718096]">Admin</small>
+                </div>
+            </div>
+    
+</header>
