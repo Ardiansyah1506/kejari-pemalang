c86bd481b8b4179ac21f2fa780de49894bbbb624
magang 5 UDI <fauziardiansyah302@gmail.com>
2024-10-21 00:46:08 +0700

    update footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="bg-green-800 text-white sm:px-6 py-2 md:py-6">
     <div class="container mx-auto px-20  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         <!-- Address -->
-        <div class="flex w-full justify-center items-center">
+        <div class="flex w-full  items-center">
             <img src="{{asset('logo-footer.png')}}" class="w-3/4 " alt="">
         </div>
         <div class="grid grid-cols-1 gap-2 px-4">
